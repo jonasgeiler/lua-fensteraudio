63e1021fc2b872ba4443d57b5968b18aa7df1bd5
Jonas Geiler <git@jonasgeiler.com>
2025-04-01 00:40:31 +0200

    refactor(demos): make wav util function more dynamic

--- demos/lib/wav.lua
+++ demos/lib/wav.lua
@@ -28,61 +28,45 @@ function wav.load(path)
 		return str, nil
 	end
 
+	---Read the next n bytes as an unsigned integer.
+	---@param bytes integer
+	---@param name string
+	---@return integer|nil, string|nil
+	---@nodiscard
+	local function read_uint(bytes, name)
+		local raw, raw_err = read_string(bytes, name)
+		if not raw then return nil, raw_err end
+		local uint = 0
+		for i = 1, bytes do
+			local byte = string.byte(raw, i)
+			if not byte then
+				return nil, 'Failed to read ' .. tostring(name) .. ' bytes, possibly early EOF'
+			end
+			uint = uint + byte * math.floor(0x100 ^ (i - 1))
+		end
+		if uint ~= uint or uint == math.huge or uint == -math.huge then
+			return nil, 'Invalid ' .. tostring(name) .. ', got Inf/NaN: ' .. tostring(uint)
+		end
+		return uint, nil
+	end
+
 	---Read the next n bytes as a signed integer.
+	---@param bytes integer
 	---@param name string
 	---@return integer|nil, string|nil
 	---@nodiscard
-	local function read_int16(name)
-		local raw, raw_err = read_string(2, name)
-		if not raw then return nil, raw_err end
-		local byte1, byte2 = string.byte(raw, 1, 2)
-		if not byte1 or not byte2 then
-			return nil, 'Failed to read ' .. tostring(name) .. ' bytes, possibly early EOF'
+	local function read_int(bytes, name)
+		local int, int_err = read_uint(bytes, name)
+		if not int then return nil, int_err end
+		local max_uint = math.floor(0x100 ^ bytes)
+		local max_int = math.floor(max_uint / 2)
+		if int >= max_int then
+			int = int - max_uint
 		end
-		local int16 = byte1 + byte2 * 0x100
-		if int16 > 0x7FFF then
-			int16 = int16 - 0x10000
+		if int ~= int or int == math.huge or int == -math.huge then
+			return nil, 'Invalid ' .. tostring(name) .. ', got Inf/NaN: ' .. tostring(int)
 		end
-		if int16 ~= int16 or int16 == math.huge or int16 == -math.huge then
-			return nil, 'Invalid ' .. tostring(name) .. ', got Inf/NaN: ' .. tostring(int16)
-		end
-		return int16, nil
-	end
-
-	---Read the next 2 bytes as an unsigned integer.
-	---@param name string
-	---@return integer|nil, string|nil
-	---@nodiscard
-	local function read_uint16(name)
-		local raw, raw_err = read_string(2, name)
-		if not raw then return nil, raw_err end
-		local byte1, byte2 = string.byte(raw, 1, 2)
-		if not byte1 or not byte2 then
-			return nil, 'Failed to read ' .. tostring(name) .. ' bytes, possibly early EOF'
-		end
-		local uint16 = byte1 + byte2 * 0x100
-		if uint16 ~= uint16 or uint16 == math.huge or uint16 == -math.huge then
-			return nil, 'Invalid ' .. tostring(name) .. ', got Inf/NaN: ' .. tostring(uint16)
-		end
-		return uint16, nil
-	end
-
-	---Read the next 4 bytes as an unsigned integer.
-	---@param name string
-	---@return integer|nil, string|nil
-	---@nodiscard
-	local function read_uint32(name)
-		local raw, raw_err = read_string(4, name)
-		if not raw then return nil, raw_err end
-		local byte1, byte2, byte3, byte4 = string.byte(raw, 1, 4)
-		if not byte1 or not byte2 or not byte3 or not byte4 then
-			return nil, 'Failed to read ' .. tostring(name) .. ' bytes, possibly early EOF'
-		end
-		local uint32 = byte1 + byte2 * 0x100 + byte3 * 0x10000 + byte4 * 0x1000000
-		if uint32 ~= uint32 or uint32 == math.huge or uint32 == -math.huge then
-			return nil, 'Invalid ' .. tostring(name) .. ', got Inf/NaN: ' .. tostring(uint32)
-		end
-		return uint32, nil
+		return int, nil
 	end
 
 	--[[ Read master "RIFF" chunk ]]
@@ -93,7 +77,7 @@ function wav.load(path)
 		return nil, 'Invalid file type chunk ID, expected "RIFF": ' .. tostring(file_type_chunk_id)
 	end
 	-- Read "FileSize"
-	local file_size, file_size_err = read_uint32('file size')
+	local file_size, file_size_err = read_uint(4, 'file size')
 	if not file_size then return nil, file_size_err end
 	if file_size < 36 then
 		return nil, 'Invalid file size, expected at least 36 bytes: ' .. tostring(file_size)
@@ -114,7 +98,7 @@ function wav.load(path)
 		local chunk_id, chunk_id_err = read_string(4, 'chunk ID')
 		if not chunk_id then return nil, chunk_id_err end
 		-- Read "BlocSize"
-		local chunk_size, chunk_size_err = read_uint32('chunk size')
+		local chunk_size, chunk_size_err = read_uint(4, 'chunk size')
 		if not chunk_size then return nil, chunk_size_err end
 		local expected_max_chunk_size = (file_size + 8) - wav_file:seek()
 		if chunk_size > expected_max_chunk_size then
@@ -156,31 +140,31 @@ function wav.load(path)
 		return nil, 'Failed to jump to "fmt " chunk: ' .. tostring(format_chunk_jump_err)
 	end
 	-- Read "AudioFormat"
-	local audio_format, audio_format_err = read_uint16('audio format')
+	local audio_format, audio_format_err = read_uint(2, 'audio format')
 	if not audio_format then return nil, audio_format_err end
 	if audio_format ~= 1 then
 		return nil, 'Invalid audio format, expected 1 (PCM integer): ' .. tostring(audio_format)
 	end
 	-- Read "NbrChannels"
-	local nbr_channels, nbr_channels_err = read_uint16('number of channels')
+	local nbr_channels, nbr_channels_err = read_uint(2, 'number of channels')
 	if not nbr_channels then return nil, nbr_channels_err end
 	if nbr_channels ~= 1 then
 		return nil, 'Invalid number of channels, expected 1 (Mono): ' .. tostring(nbr_channels)
 	end
 	-- Read "Frequency"
-	local sample_rate, sample_rate_err = read_uint32('sample rate')
+	local sample_rate, sample_rate_err = read_uint(4, 'sample rate')
 	if not sample_rate then return nil, sample_rate_err end
 	if sample_rate ~= 44100 then
 		return nil, 'Invalid sample rate, expected 44100 (Hz): ' .. tostring(sample_rate)
 	end
 	-- Read "BytePerSec"
-	local byte_per_sec, byte_per_sec_err = read_uint32('byte per second')
+	local byte_per_sec, byte_per_sec_err = read_uint(4, 'byte per second')
 	if not byte_per_sec then return nil, byte_per_sec_err end
 	-- Read "BytePerBloc"
-	local byte_per_chunk, byte_per_chunk_err = read_uint16('byte per chunk')
+	local byte_per_chunk, byte_per_chunk_err = read_uint(2, 'byte per chunk')
 	if not byte_per_chunk then return nil, byte_per_chunk_err end
 	-- Read "BitsPerSample"
-	local bits_per_sample, bits_per_sample_err = read_uint16('bits per sample')
+	local bits_per_sample, bits_per_sample_err = read_uint(2, 'bits per sample')
 	if not bits_per_sample then return nil, bits_per_sample_err end
 	if bits_per_sample ~= 16 then
 		return nil, 'Invalid bits per sample, expected 16: ' .. tostring(bits_per_sample)
@@ -206,7 +190,7 @@ function wav.load(path)
 	local samples = {} ---@type number[]
 	local samples_index = 1
 	while wav_file:seek() < chunk_end do
-		local integer_sample, sample_err = read_int16('sample')
+		local integer_sample, sample_err = read_int(2, 'sample')
 		if not integer_sample then return nil, sample_err end
 		local float_sample = integer_sample / 32768
 		samples[samples_index] = float_sample
